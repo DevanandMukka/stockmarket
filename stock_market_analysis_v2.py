a5351bb8f7c8d37b8c9469fd00e6de48bb48c5c7
DevanandMukka <45567789+DevanandMukka@users.noreply.github.com>
2025-11-06 13:06:12 +0530

    Update stock_market_analysis_v2.py

--- stock_market_analysis_v2.py
+++ stock_market_analysis_v2.py
@@ -373,11 +373,11 @@ else:
                     <span style="color:#047857;">S3={prev_S3:.2f}</span>, 
                     <span style="font-weight:600;color:#374151;">Pivot Width :</span>
                     <span style="color:#DC2626;font-weight:bold;">Pivot Width (R3 - S3) ={curr_cm_diff:.2f}</span>
-                    <br>
+                    <br><br><br>
                     <span style="font-weight:bold;text-decoration:underline;font-size:19px;color:#1E3A8A;
                     background-color:#DBEAFE;padding:3px 8px;border-radius:6px;display:inline-block;margin-bottom:5px;">
                     📅 Details for Next Trading Day ({next_date.strftime('%d-%b-%Y')})
-                    </span><br><br>
+                    </span><br>
                     <span style="font-weight:600;color:#374151;">Details :</span>
                     <span style="color:#1E40AF;">R3={next_R3:.2f}</span>, 
                     <span style="color:#047857;">S3={next_S3:.2f}</span>, 
@@ -413,6 +413,7 @@ else:
                           xaxis_title="Date", yaxis_title="Price",
                           xaxis_rangeslider_visible=False)
     st.plotly_chart(fig_cam, use_container_width=True)
+
 
 
 
