0b3ac3a853e46ec3d9f8398c6a4b898b6d20c7e6
DevanandMukka <45567789+DevanandMukka@users.noreply.github.com>
2025-12-09 09:27:13 +0530

    Update stock_market_analysis_v2.py

--- stock_market_analysis_v2.py
+++ stock_market_analysis_v2.py
@@ -474,9 +474,9 @@ else:
     
         bullish_comment = f"""
         <b>However, there are a couple of factors that must be in place in order for a "buy the dip" opportunity to exist.<br>
-        <b><u>First</u></b>, price should open the {data_freq[:-2].lower()} above the central pivot range.<br>
+        <b><u>First</u></b>, price should open the day above the central pivot range.<br>
         <span style="color:{'#16a34a' if first_fact else '#404040'};">(Close={curr_open:.2f}; CPR: BC={next_bc:.2f} TC={next_tc:.2f})</span><br>
-        <b><u>Second</u></b>, the prior {data_freq[:-2].lower()}'s closing price should fall above the prior {data_freq[:-2].lower()}'s central pivot range.<br>
+        <b><u>Second</u></b>, the prior day's closing price should fall above the prior {data_freq[:-2].lower()}'s central pivot range.<br>
         <span style="font-size:24px; color:{'#16a34a' if third_fact else '#404040'};">2nd condition satisfied: {"Yes" if third_fact else "No"}</span><br><br>
         If both of these factors pass the test, the market is likely primed for another "buy the dip" opportunity (reverse for shorts).</b>
         """
@@ -608,3 +608,4 @@ else:
                 🔍 No Double Pivot Hot Zone (DPZ) detected for the next session within the current tolerance ({tolerance_pct*100:.2f}% of price).
             </div>
         """, unsafe_allow_html=True)
+
